114049a1681a0d82c96555546fdbd1003217f145
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-03-31 00:26:45 +0300

    entry commit

---
+++ src/dev/palgogo/Main.java
@@ -0,0 +1,13 @@
+package dev.palgogo;
+
+import org.opencv.core.Core;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(Core.VERSION);
+        System.out.println(Core.VERSION_MAJOR);
+        System.out.println(Core.VERSION_MINOR);
+    }
+}
