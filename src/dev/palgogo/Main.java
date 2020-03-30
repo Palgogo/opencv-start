ef4c3e44254a536f705f136d1d7e9d1128581014
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-03-31 01:19:38 +0300

    Learn how work with point

--- src/dev/palgogo/Main.java
+++ src/dev/palgogo/Main.java
@@ -1,13 +1,16 @@
 package dev.palgogo;
 
 import org.opencv.core.Core;
+import org.opencv.core.Point;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(Core.VERSION);
-        System.out.println(Core.VERSION_MAJOR);
-        System.out.println(Core.VERSION_MINOR);
+        PointExplorer pointExplorer = new PointExplorer();
+
+        pointExplorer.isPointInsideRectangleTest();
+
     }
+
 }
---
+++ src/dev/palgogo/PointExplorer.java
@@ -0,0 +1,30 @@
+package dev.palgogo;
+
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+
+public class PointExplorer {
+
+    public void point(){
+        Point point = new Point(new double[] {10.0, 0.5});
+
+        Point point2 = point;
+
+        point2.x=20;
+
+        System.out.println(point.x + "  ," + point.y );
+        System.out.println(point2.x + "  ," + point2.y );
+        point.x= 10;
+        Point clone = point2.clone();
+        System.out.println("point clone: " + clone);
+    }
+
+    public void isPointInsideRectangleTest(){
+        Point p = new Point(9.0, 5.0);
+        Rect rect = new Rect(0, 0, 10, 10);
+        Rect rect2 = new Rect(10, 5, 10, 10);
+
+        System.out.println(p.inside(rect));
+        System.out.println(p.inside(rect2));
+    }
+}
