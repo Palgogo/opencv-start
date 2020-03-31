39ca5f6ddeb60a3fdfacd546da722d5206324128
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-03-31 20:36:07 +0300

    daig method

--- src/dev/palgogo/matrix/MatrixExplorer.java
+++ src/dev/palgogo/matrix/MatrixExplorer.java
@@ -6,11 +6,18 @@ import org.opencv.core.Mat;
 public class MatrixExplorer {
 
     public void simpleMatrix() {
-        Mat m = new Mat(3, 2, CvType.CV_8UC1);
+        Mat m = new Mat(1, 3, CvType.CV_8UC1);
+        double n = 1.0;
+
+        for (int i = 0; i < m.rows(); i++) {
+            for (int j = 0; j < m.cols(); j++) {
+                m.put(i, j, n++);
+            }
+        }
         System.out.println(m.dump());
-        System.out.println(m.cols());
-        System.out.println(m.rows());
-        System.out.println(m.width());
-        System.out.println(m.height());
+
+        Mat mat2 = Mat.diag(m);
+        System.out.println(mat2.dump());
+
     }
 }
