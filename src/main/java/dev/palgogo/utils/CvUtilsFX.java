eb1c15b4275c9c673f197e9cd43860b78d1c76fd
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-04-05 15:06:30 +0300

    add showImage to Fx utils.

--- src/main/java/dev/palgogo/utils/CvUtilsFX.java
+++ src/main/java/dev/palgogo/utils/CvUtilsFX.java
@@ -1,7 +1,11 @@
 package dev.palgogo.utils;
 
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
@@ -67,4 +71,34 @@ public class CvUtilsFX {
         return m;
     }
 
+    public static void showImage(Mat img, String title) {
+        Image im = matToImageFX(img);
+
+        Stage window = new Stage();
+        ScrollPane sp = new ScrollPane();
+        ImageView iv = new ImageView();
+
+        if (im != null) {
+            iv.setImage(im);
+
+            if (im.getWidth() < 1000) {
+                sp.setPrefWidth(im.getWidth() + 5);
+            } else sp.setPrefWidth(1000.0);
+
+            if (im.getHeight() < 700) {
+                sp.setPrefHeight(im.getHeight() + 5);
+            } else sp.setPrefHeight(700.0);
+        }
+
+        sp.setContent(iv);
+        sp.setPannable(true);
+
+        BorderPane box = new BorderPane();
+        box.setCenter(sp);
+
+        Scene scene = new Scene(box);
+        window.setScene(scene);
+        window.setTitle(title);
+        window.show();
+    }
 }
