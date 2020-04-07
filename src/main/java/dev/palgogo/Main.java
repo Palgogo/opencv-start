e65e524a9d1650fbc9ff00b8be42fdd57a3c9f13
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-04-07 15:06:53 +0300

    add find object by color method

--- src/main/java/dev/palgogo/Main.java
+++ src/main/java/dev/palgogo/Main.java
@@ -1,6 +1,6 @@
 package dev.palgogo;
 
-import dev.palgogo.chapter7.FilterApplying;
+import dev.palgogo.chapter8.FindingObjects;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -41,7 +41,7 @@ public class Main extends Application {
     }
 
     private void onClickButton(ActionEvent event) {
-        new FilterApplying().medianBlurApplying();
+        new FindingObjects().findingDifference();
     }
 
     //todo add seting path to image
--- src/main/java/dev/palgogo/chapter8/FindingObjects.java
+++ src/main/java/dev/palgogo/chapter8/FindingObjects.java
@@ -1,15 +1,16 @@
 package dev.palgogo.chapter8;
 
+import dev.palgogo.labs.Third;
 import dev.palgogo.utils.CvUtils;
-import dev.palgogo.utils.CvUtilsFX;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+
+import static dev.palgogo.utils.CvUtilsFX.showImage;
 
 public class FindingObjects {
 
@@ -23,24 +24,24 @@ public class FindingObjects {
         }
         Mat imgGray = new Mat();
         Imgproc.cvtColor(img, imgGray, Imgproc.COLOR_BGR2GRAY);
-        CvUtilsFX.showImage(imgGray, "GRAY");
+        showImage(imgGray, "GRAY");
         Mat edges = new Mat();
         Imgproc.Canny(imgGray, edges, 80, 200, 3, true);
         //can be cool with slider
-        CvUtilsFX.showImage(edges, "Canny");
+        showImage(edges, "Canny");
         Mat img3 = new Mat();
         Imgproc.threshold(imgGray, img3, 100, 255,
                 Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
         Mat edges2 = new Mat();
         Imgproc.Canny(img3, edges2, 80, 200);
-        CvUtilsFX.showImage(edges2, "Canny + THRESH_OTSU");
+        showImage(edges2, "Canny + THRESH_OTSU");
         Mat img4 = new Mat();
         Imgproc.adaptiveThreshold(imgGray, img4, 255,
                 Imgproc.ADAPTIVE_THRESH_MEAN_C,
                 Imgproc.THRESH_BINARY, 3, 5);
         Mat edges3 = new Mat();
         Imgproc.Canny(img4, edges3, 80, 200);
-        CvUtilsFX.showImage(edges3, "Canny + adaptiveThreshold");
+        showImage(edges3, "Canny + adaptiveThreshold");
         img.release();
         img3.release();
         img4.release();
@@ -62,7 +63,7 @@ public class FindingObjects {
         Imgproc.cvtColor(img, imgGray, Imgproc.COLOR_BGR2GRAY);
         Mat edges = new Mat();
         Imgproc.Canny(imgGray, edges, 100, 200);
-        CvUtilsFX.showImage(edges, "Canny");
+        showImage(edges, "Canny");
         Mat edgesCopy = edges.clone(); //create Copy
         ArrayList<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
@@ -74,11 +75,65 @@ public class FindingObjects {
         System.out.println(hierarchy.dump());
 
         Imgproc.drawContours(img, contours, -1, CvUtils.COLOR_WHITE);
-        CvUtilsFX.showImage(img, "drawContours");
+        showImage(img, "drawContours");
         img.release();
         imgGray.release();
         edges.release();
         edgesCopy.release();
         hierarchy.release();
+    }
+
+    public void findObjectByColor() {
+        Mat img = Third.getMatWithImage();
+        showImage(img, "Original");
+        Mat hsv = new Mat();
+        Imgproc.cvtColor(img, hsv, Imgproc.COLOR_BGR2HSV);
+
+        Mat h = new Mat();
+        Core.extractChannel(hsv, h, 0);
+
+        Mat img2 = new Mat();
+        Core.inRange(h, new Scalar(40), new Scalar(80), img2);
+        showImage(img2, "Green");
+
+        Core.inRange(h, new Scalar(100), new Scalar(140), img2);
+        showImage(img2, "Blue");
+
+        Core.inRange(hsv, new Scalar(0, 200, 200),
+                new Scalar(20, 256, 256), img2);
+        showImage(img2, "Red");
+
+        Core.inRange(hsv, new Scalar(0, 0, 0),
+                new Scalar(0, 0, 50), img2);
+        showImage(img2, "Black");
+
+        img.release();
+        img2.release();
+        hsv.release();
+        h.release();
+    }
+
+    public void findingDifference() {
+        Mat img = Third.getMatWithImage();
+        showImage(img, "Original");
+
+        Mat img2 = img.clone();
+        Imgproc.circle(img2, new Point(200, 200), 50, CvUtils.COLOR_RED,
+                Core.FILLED);
+        showImage(img2, "Original + Circle");
+
+        Mat img3 = new Mat();
+        Core.absdiff(img2, img, img3);
+        showImage(img3, "Difference");
+
+        Mat img4 = new Mat();
+        Imgproc.cvtColor(img3, img4, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.threshold(img4, img4, 1, 255, Imgproc.THRESH_BINARY);
+        showImage(img4, "Thereshold");
+
+        img.release();
+        img2.release();
+        img3.release();
+        img4.release();
     }
 }
