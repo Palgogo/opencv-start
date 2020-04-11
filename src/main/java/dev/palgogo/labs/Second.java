a73e2223633afac38d72f8f2c6be7752e9232456
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-04-11 18:53:19 +0300

    add documentation for second lab

---
+++ src/main/java/dev/palgogo/labs/Second.java
@@ -0,0 +1,10 @@
+package dev.palgogo.labs;
+
+public class Second {
+
+    /**
+     *  ./darknet detect cfg/yolov3.cfg yolov3.weights data/<photo>.<extension>
+     *     after running this in darknet folder we have prediction.jpg file
+     *
+     */
+}
