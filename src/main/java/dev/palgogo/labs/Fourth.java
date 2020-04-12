1a1f82d3c155fdedcffabdc268c1df08427c025b
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-04-12 10:46:44 +0300

    add haarcascade for frontal face

---
+++ src/main/java/dev/palgogo/labs/Fourth.java
@@ -0,0 +1,12 @@
+package dev.palgogo.labs;
+
+import org.opencv.objdetect.CascadeClassifier;
+
+public class Fourth {
+
+    public void facesOnVideo() {
+
+        CascadeClassifier face_detector = new CascadeClassifier();
+
+    }
+}
