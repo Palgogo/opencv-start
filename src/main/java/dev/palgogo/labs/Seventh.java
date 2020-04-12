f703af67ee741aadf6e5de93de0d0f5195f72836
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-04-12 18:35:38 +0300

    Merge branch '' of D:\Projects\opencv-samples\opencv-start with conflicts.

--- src/main/java/dev/palgogo/labs/Seventh.java
+++ src/main/java/dev/palgogo/labs/Seventh.java
@@ -15,7 +15,7 @@ public class Seventh {
     private static final String UPPERBODY = "/haarcascades/haarcascade_upperbody.xml";
     private static final String PROFILEFACE = "/haarcascades/haarcascade_profileface.xml";
     private static final String FRONTAL_FACE = "/haarcascades/haarcascade_frontalface_alt2.xml";
-    private static final String PEOPLES = "/haarcascades/cascadG.xml";
+    private static final String PEOPLES = "/haarcascades/case.xml";
 
     public void detectPeopleOnImage() {
         final Path pathToResources = Paths.get("src", "main", "resources");
@@ -29,7 +29,7 @@ public class Seventh {
             return;
         }
 
-        Path path = Paths.get("src", "main", "resources", "photos", "peoples2.jpg");
+        Path path = Paths.get("src", "main", "resources", "photos", "peoples3.jpg");
         Path fullResourcePath = path.toAbsolutePath();
         Mat frame = Imgcodecs.imread(fullResourcePath.toString());
         if (frame.empty()) {
@@ -38,10 +38,10 @@ public class Seventh {
         }
 
         final MatOfRect faces = new MatOfRect();
-        Imgproc.resize(frame, frame, new Size(), 0.5, 0.5, Imgproc.ADAPTIVE_THRESH_MEAN_C);
+        Imgproc.resize(frame, frame, new Size(), 0.6, 0.6, Imgproc.ADAPTIVE_THRESH_MEAN_C);
 
         //TODO should work with another parameters
-        face_detector.detectMultiScale(frame, faces, 1.1, 2, 4);
+        face_detector.detectMultiScale(frame, faces, 1.15, 2, 0);
         for (Rect r : faces.toList()) {
             Imgproc.rectangle(frame, new Point(r.x, r.y),
                     new Point(r.x + r.width, r.y + r.height),
